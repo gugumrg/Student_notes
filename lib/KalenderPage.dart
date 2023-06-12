a06d5070ac1c3e821127ce517be59204858222b8
gugumrg <grahmatgumilar@gmail.com>
2023-06-12 20:26:10 +0700

    Tambah Kalender

--- lib/KalenderPage.dart
+++ lib/KalenderPage.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class KalenderPage extends StatelessWidget {
+class KalenderPage extends StatefulWidget {
+  const KalenderPage({Key? key}) : super(key: key);
+
+  @override
+  _KalenderPageState createState() => _KalenderPageState();
+}
+
+class _KalenderPageState extends State<KalenderPage> {
+  late CalendarFormat _calendarFormat;
+  late DateTime _focusedDay;
+  late DateTime _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarFormat = CalendarFormat.month;
+    _focusedDay = DateTime.now();
+    _selectedDay = DateTime.now();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Kalender'),
       ),
-      body: const Center(
-        child: Text('Halaman Kalender'),
+      body: Column(
+        children: [
+          TableCalendar(
+            calendarFormat: _calendarFormat,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            firstDay: DateTime.utc(2000),
+            lastDay: DateTime.utc(2030),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Selected Date: ${_selectedDay.toString()}',
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
